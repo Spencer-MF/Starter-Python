f2eff5b967c2bc453419eebf805e2688af2f6d2c
Spencer-MF <spencermichealfranklin@gmail.com>
2024-06-26 00:19:27 -0700

    fleshing out odd or even

--- Mac code/oddoreven.py
+++ Mac code/oddoreven.py
@@ -1,14 +1,28 @@
 from Nicetohaves import Utility
 
 def main():
-    for i in range(101):
-        if is_odd(i):
-            print(i, 'odd')
+    i = input('Would you like 1. to know if a number is odd or even or 2. all numbers to your number that are odd or even?\n')
+    if i in ['1', '1.']:
+        n = int(input('What number would you like to now if odd or even? '))
+        odd = is_odd(n)
+        if odd:
+            print(n, 'is odd')
         else:
-            print(i, 'even')
+            print(n, 'is even')
+    elif i in ['2', '2.']:
+        upto()
+        
 
 def is_odd(n):
     odd_check = n % 2
     return odd_check == 1
+    
+def upto():
+    n = int(input('to what number do you want to know the odds and even: '))
+    for i in range(n + 1):
+        if is_odd(i):
+            print(i, 'odd')
+        else:
+            print(i, 'even')
     
 Utility.restart(main)
